2ca2d9cd7b2e68d69b11cf24e93329e65c5f0f5c
Virgilio Padilla Ruiz <vpadillar8@gmail.com>
2026-08-14 11:43:58 -0500

    ajustes administrables

--- web/modules/custom/dermau_core/src/Plugin/Block/SliderBlock.php
+++ web/modules/custom/dermau_core/src/Plugin/Block/SliderBlock.php
@@ -71,13 +71,6 @@ class SliderBlock extends BlockBase
   public function blockSubmit($form, FormStateInterface $form_state)
   {
     $float_chat_image = $form_state->getValue('float_chat_image');
-    \Drupal::logger('dermaU')->info(
-      'float_chat_image[0]: @der, float_chat_image: @red',
-      [
-        '@der' => $float_chat_image[0],
-        '@red' => $float_chat_image,
-      ]
-    );
 
     if (!empty($float_chat_image[0])) {
       $file = File::load($float_chat_image[0]);
